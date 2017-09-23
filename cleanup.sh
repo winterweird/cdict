5b01179a249d32ca92cf435955537c29b9e4498f
Vegard Itland <Vegard.Itland@student.uib.no>
2017-09-24 01:44:45 +0200

    Make lib installable, improve cleanup
    Modified Makefile.am to install library on make install, as well as modifying
    cleanup.sh to also remove .o files generated in the src folder and the .dirstamp
    file which for some reason also sometimes appears there.

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,4 @@
 rm -rf autom4te.cache/ src/.deps/
 rm Makefile aclocal.m4 cdist-0.1.tar.gz \
     compile config.log config.status depcomp \
-    install-sh missing
+    install-sh missing src/.dirstamp src/*.o
