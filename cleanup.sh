827434a2a67cf3e71ce5d56353e3e206b95d69c6
Vegard Itland <Vegard.Itland@student.uib.no>
2017-09-24 03:12:55 +0200

    Modify cleanup script
    I decided to just let cleanup scrap everything, including the configure and
    Makefile.in files, since they're basically junk without the other files
    configure depends on, which I'm already removing. Also updated it to clean up in
    the new files and stuff appearing due to the makefile workload redistribution.

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,5 @@
-rm -rf autom4te.cache/ src/.deps/
-rm Makefile aclocal.m4 cdist-0.1.tar.gz \
-    compile config.log config.status depcomp \
-    install-sh missing src/.dirstamp src/*.o
+rm -rf autom4te.cache/ src/.deps/ examples/.deps/
+rm Makefile Makefile.in configure aclocal.m4 cdist-0.1.tar.gz \
+    compile config.log config.status depcomp install-sh missing
+rm examples/Makefile examples/Makefile.in
+rm include/Makefile include/Makefile.in
